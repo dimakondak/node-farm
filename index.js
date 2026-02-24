a8a304374bdf58ad1b84f84a6ab954fa6de46da9
kondak <dmytro.kondak@nixs.com>
2026-02-24 09:06:38 +0100

    configure prettier

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ const Path = {
 };
 
 const cardTemplate = fs.readFileSync(
-    `${__dirname}/templates/template-card.html`, 'utf8');
+    `${__dirname}/templates/template-card.html`,
+    'utf8'
+);
 const overviewTemplate = fs.readFileSync(
-    `${__dirname}/templates/template-overview.html`, 'utf8');
+    `${__dirname}/templates/template-overview.html`,
+    'utf8'
+);
 const productTemplate = fs.readFileSync(
-    `${__dirname}/templates/template-product.html`, 'utf8');
+    `${__dirname}/templates/template-product.html`,
+    'utf8'
+);
 
 const products = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
 const productsJson = JSON.parse(products);
@@ -24,35 +30,46 @@ const productsJson = JSON.parse(products);
 const slugsMap = new Map(
     productsJson.map(({ id, productName }) => [
         slugify(productName, { lower: true }),
-        id
+        id,
     ])
 );
-const templateCreator = new TemplateCreator(slugsMap)
+const templateCreator = new TemplateCreator(slugsMap);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
 
     switch (pathname) {
         case Path.OVERVIEW:
-            const productCardTemplate =
-                productsJson
-                .map(product => templateCreator
-                .createProductCardTemplate(cardTemplate, product))
+            const productCardTemplate = productsJson
+                .map((product) =>
+                    templateCreator.createProductCardTemplate(
+                        cardTemplate,
+                        product
+                    )
+                )
                 .join('');
 
             res.writeHead(200, {
                 'Content-type': 'text/html',
             });
-            res.end(templateCreator.createOverviewTemplate(overviewTemplate,
-                productCardTemplate));
+            res.end(
+                templateCreator.createOverviewTemplate(
+                    overviewTemplate,
+                    productCardTemplate
+                )
+            );
             break;
         case Path.PRODUCT:
             const product = productsJson[slugsMap.get(query.id)];
             res.writeHead(200, {
                 'Content-type': 'text/html',
             });
-            res.end(templateCreator
-            .createProductCardTemplate(productTemplate, product));
+            res.end(
+                templateCreator.createProductCardTemplate(
+                    productTemplate,
+                    product
+                )
+            );
             break;
         case Path.API:
             res.writeHead(200, {
@@ -66,7 +83,6 @@ const server = http.createServer((req, res) => {
             });
             res.end('<h1>Not Found!</h1>');
     }
-
 });
 
 server.listen(8080, '127.0.0.1', () => {
--- modules/TemplateCreator.js
+++ modules/TemplateCreator.js
@@ -1,19 +1,24 @@
 function TemplateCreator(slugsMap) {
-    this.slugsMap = slugsMap
-    this.createProductCardTemplate = (cardTemplate, product) => cardTemplate
-    .replace(/{%PRODUCT_NAME%}/g, product?.productName)
-    .replace(/{%ICON%}/g, product?.image)
-    .replace(/{%PRICE%}/g, product?.price)
-    .replace(/{%FROM%}/g, product?.from)
-    .replace(/{%NUTRIENTS_NAME%}/g, product?.nutrients)
-    .replace(/{%QUANTITY%}/g, product?.quantity)
-    .replace(/{%PRICE%}/g, product?.price)
-    .replace(/{%DESCRIPTION%}/g, product?.description)
-    .replace(/{%NOT_ORGANIC%}/g, product?.organic ? '' : 'not-organic')
-    .replace(/{%ID%}/g, this.slugsMap.entries().find(([, value]) => value === product?.id)[0]);
-    this.createOverviewTemplate = (overviewTemplate,
-        productCardTemplate) => overviewTemplate
-    .replace('{%PRODUCT_CARDS%}', productCardTemplate);
+    this.slugsMap = slugsMap;
+    this.createProductCardTemplate = (cardTemplate, product) =>
+        cardTemplate
+            .replace(/{%PRODUCT_NAME%}/g, product?.productName)
+            .replace(/{%ICON%}/g, product?.image)
+            .replace(/{%PRICE%}/g, product?.price)
+            .replace(/{%FROM%}/g, product?.from)
+            .replace(/{%NUTRIENTS_NAME%}/g, product?.nutrients)
+            .replace(/{%QUANTITY%}/g, product?.quantity)
+            .replace(/{%PRICE%}/g, product?.price)
+            .replace(/{%DESCRIPTION%}/g, product?.description)
+            .replace(/{%NOT_ORGANIC%}/g, product?.organic ? '' : 'not-organic')
+            .replace(
+                /{%ID%}/g,
+                this.slugsMap
+                    .entries()
+                    .find(([, value]) => value === product?.id)[0]
+            );
+    this.createOverviewTemplate = (overviewTemplate, productCardTemplate) =>
+        overviewTemplate.replace('{%PRODUCT_CARDS%}', productCardTemplate);
 }
 
-module.exports = TemplateCreator
+module.exports = TemplateCreator;
