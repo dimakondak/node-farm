1f94fcd3d5e3b227c4ee821df8c1e8ea905c3538
kondak <dmytro.kondak@nixs.com>
2026-03-04 12:43:37 +0100

    add db connection

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+const db_uri = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_KEY
+);
+
+mongoose
+  .connect(db_uri)
+  .then((connection) => {
+    console.log('Successfully connected to database', connection);
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
