4377e9c9c0f346f4dbbb2270ad11ef8183a0f37f
kondak <dmytro.kondak@nixs.com>
2026-03-10 18:10:36 +0100

    restore packages

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ process.loadEnvFile();
 const app = express();
 
 if (process.env.NODE_ENV === 'dev') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 app.set('query parser', 'extended');
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
-    console.log(req.url);
+  console.log(req.url);
 
-    next();
+  next();
 });
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
+  req.requestTime = new Date().toISOString();
 
-    next();
+  next();
 });
 
 // Routes
@@ -29,7 +29,7 @@ app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.get('/hello-world', (req, res) => {
-    res.status(200).send('Hello World!');
+  res.status(200).send('Hello World!');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -20,5 +20,5 @@ mongoose
   });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
