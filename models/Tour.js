c4ce5e049282e60a2311cac9ef7b82ad5a65e9ac
kondak <dmytro.kondak@nixs.com>
2026-03-11 16:03:47 +0100

    add pre-save middleware

--- models/Tour.js
+++ models/Tour.js
@@ -1,38 +1,66 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    duration: { type: Number, required: [true, 'A tour must have a duration'] },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    price: { type: Number, required: [true, 'A tour must have a price'] },
+    priceDiscount: Number,
+    ratingAverage: { type: Number, default: 4.5 },
+    ratingQuantity: { type: Number, default: 0 },
+    summary: { type: String, trim: true },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: { type: Date, default: Date.now(), select: false },
+    startDates: [Date],
   },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  duration: { type: Number, required: [true, 'A tour must have a duration'] },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-  priceDiscount: Number,
-  ratingAverage: { type: Number, default: 4.5 },
-  ratingQuantity: { type: Number, default: 0 },
-  summary: { type: String, trim: true },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: { type: Date, default: Date.now(), select: false },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+/**
+ * Middleware to handle pre-save operations. Runs only before .save() and .create()
+ * does NOT run before .insertMany()
+ */
+tourSchema.pre('save', function () {
+  this.slug = slugify(this.name, { lower: true });
+});
+
+tourSchema.pre('save', function () {
+  console.log('Saving tour');
+});
+
+tourSchema.post('save', async function (tour) {
+  console.log('Tour was saved', tour);
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
