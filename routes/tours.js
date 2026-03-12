6777c5d22af66eabe47b913b5903e8e481ba28cd
kondak <dmytro.kondak@nixs.com>
2026-03-12 11:35:29 +0100

    reorder routes

--- routes/tours.js
+++ routes/tours.js
@@ -12,14 +12,14 @@ const {
 
 const router = express.Router();
 
-router.route('/').get(getTours).post(createTour);
-
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
 router.route('/top-5-cheap').get(aliasTopTours, getTours);
 
 router.route('/stats').get(getTourStats);
 
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
+
+router.route('/').get(getTours).post(createTour);
+
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
