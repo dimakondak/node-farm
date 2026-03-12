c45b3ad36a77393c6192d4b9868398347eb778d8
kondak <dmytro.kondak@nixs.com>
2026-03-12 11:59:13 +0100

    add built-in validators

--- models/Tour.js
+++ models/Tour.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxLength: [40, 'A tour name must have less or equal then 40 characters'],
+      minLength: [10, 'A tour name must have more or equal then 10 characters'],
     },
     slug: String,
     description: {
@@ -23,10 +25,19 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     price: { type: Number, required: [true, 'A tour must have a price'] },
     priceDiscount: Number,
-    ratingAverage: { type: Number, default: 4.5 },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'A tour must have a rating between 1 and 5'],
+      max: [5, 'A tour must have a rating between 1 and 5'],
+    },
     ratingQuantity: { type: Number, default: 0 },
     summary: { type: String, trim: true },
     imageCover: {
