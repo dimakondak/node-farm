6d3e9c07537cb034c5599da1fa59868e2191a891
kondak <dmytro.kondak@nixs.com>
2026-04-07 17:09:31 +0200

    add GeoJSON support to Tour model and introduce LEAD_GUIDE role to UserRole model

--- models/Tour.js
+++ models/Tour.js
@@ -62,6 +62,32 @@ const tourSchema = new mongoose.Schema(
     createdAt: { type: Date, default: Date.now(), select: false },
     startDates: [Date],
     secretTour: { type: Boolean, default: false },
+    /**
+     * GeoJSON
+     */
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    location: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
