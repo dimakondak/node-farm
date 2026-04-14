afab55eb0572fcaca75294c27c2189be33a93a63
kondak <dmytro.kondak@nixs.com>
2026-04-14 15:06:27 +0200

    fix: adjust stats route placement to ensure proper middleware execution sequence

--- routes/tours.js
+++ routes/tours.js
@@ -16,10 +16,10 @@ const { UserRole } = require('../UserRole');
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getTours);
-router.route('/stats').get(restrictTo([UserRole.ADMIN]), getTourStats);
 router.route('/').get(getTours);
 
 router.use(protect);
+router.route('/stats').get(restrictTo([UserRole.ADMIN]), getTourStats);
 router
   .route('/monthly-plan/:year')
   .get(restrictTo([UserRole.ADMIN]), getMonthlyPlan);
